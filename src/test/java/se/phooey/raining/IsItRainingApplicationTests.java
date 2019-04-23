d8bf1b8c95f53aa16d0b37beca9433419a5e845a
Mattias Sjödin <matte.sjodin@gmail.com>
2019-04-23 09:44:34 +0200

    Made Spring unit test more concrete

--- src/test/java/se/phooey/raining/IsItRainingApplicationTests.java
+++ src/test/java/se/phooey/raining/IsItRainingApplicationTests.java
@@ -1,20 +1,30 @@
 package se.phooey.raining;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import se.phooey.raining.weather.DarkSkyWeatherProvider;
+import se.phooey.raining.weather.WeatherProvider;
+
 /**
- * Simple test to make sure the application context loads correctly, if not it will be caught already here
+ * Simple test to make sure the application context loads correctly and that the
+ * expected beans are injected, if not it will be caught already here.
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class IsItRainingApplicationTests {
 
+	@Autowired
+	WeatherProvider wp;
+
 	@Test
-	public void contextLoads() {
+	public void contextLoadsAndWeatherProviderBeanIsInjected() {
+		assertThat(wp).isInstanceOf(DarkSkyWeatherProvider.class);
 	}
 
 }
-
